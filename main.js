aad7290047d6983b0c5bef0734226668c76dbca4
username <username@example.com>
2019-08-30 20:10:41 +0900

    Create DOM constant

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
  *   - textContentを使う
  *     - 「JSで変更した後のタイトル」というテキストをセットする
  */
-
+const changeTextContent = document.getElementById('title');
+changeTextContent.textContent = 'JSで変更した後のタイトル';
 
 /**
  * 課題2: p要素でid属性値が `description` のテキスト内容を変更する
@@ -11,19 +12,22 @@
  *     - 「JSで変更した後の説明文」というテキストをセットする
  *   - strong要素を使って「説明文」を強調する
  */
+const changeInnerHTML = document.getElementById('description');
+changeInnerHTML.innerHTML = 'JSで変更した後の<strong>説明文</strong>';
 
-
- /**
-  * 課題3: img要素でid属性値が 'profile' のsrc属性値を変更する
-  *   - セットする画像は以下のいずれかで対応する
-  *     - 自分で用意する
-  *     - 「https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg」を使う
-  */
-
+/**
+ * 課題3: img要素でid属性値が 'profile' のsrc属性値を変更する
+ *   - セットする画像は以下のいずれかで対応する
+ *     - 自分で用意する
+ *     - 「https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg」を使う
+ */
+const setImgSrc = document.getElementById('profile');
+setImgSrc.setAttribute('src', 'https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg');
 
 /**
  * 課題4: 課題1で作成したh1要素のDOMにクラス属性値をセットする
  *   - セットするクラス属性値はstyle.cssの中身を参考にする
  *     - 背景色がh1要素の背景色が黄色になればOKです
  */
-
+const setClass = document.getElementById('title');
+setClass.className = 'bg-yellow';
