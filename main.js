97dfd46726034989fd4b0b1cda7c549fdbaeb644
mi-ko-cute <thx-mail@worysolve.com>
2019-10-01 12:31:31 +0900

    フロントエンド２つめのエクササイズに取り組みました

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
  *   - textContentを使う
  *     - 「JSで変更した後のタイトル」というテキストをセットする
  */
-
+const titleElement = document.getElementById('title');
+titleElement.textContent = 'JSで変更した後のタイトル';
 
 /**
  * 課題2: p要素でid属性値が `description` のテキスト内容を変更する
@@ -11,7 +12,8 @@
  *     - 「JSで変更した後の説明文」というテキストをセットする
  *   - strong要素を使って「説明文」を強調する
  */
-
+const descriptionElement = document.getElementById('description');
+descriptionElement.innerHTML = 'JSで変更した後の<strong>説明文</strong>';
 
  /**
   * 課題3: img要素でid属性値が 'profile' のsrc属性値を変更する
@@ -19,11 +21,12 @@
   *     - 自分で用意する
   *     - 「https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg」を使う
   */
-
+const profileElement = document.getElementById('profile');
+profileElement.setAttribute('src', 'https://www.pakutaso.com/shared/img/thumb/TOMnemuiDSC01757_TP_V1.jpg');
 
 /**
  * 課題4: 課題1で作成したh1要素のDOMにクラス属性値をセットする
  *   - セットするクラス属性値はstyle.cssの中身を参考にする
  *     - 背景色がh1要素の背景色が黄色になればOKです
  */
-
+titleElement.className = 'bg-yellow'
